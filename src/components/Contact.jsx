6c47b447f0f1bc06f1f6c77f28a5af3b9c1b6f84
Manish Vishwakarma <hbxmanish@gmail.com>
2023-05-01 11:34:54 +0000

    final completion done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -31,18 +31,17 @@ const Contact = () => {
     e.preventDefault();
     setLoading(true);
 
-    emailjs
-      .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+    emailjs.send(
+        'service_pw1uj0p',
+        'template_ohxzewr',
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Manish Vishwakarma",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "hbxmanish@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        '8LX_qwmvMMVWim8ya'
       )
       .then(
         () => {
